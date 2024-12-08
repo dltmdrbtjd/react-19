63e2b8d535570545b573066a27f90d6165fb9373
dltmdrbtjd <dltmdrbtjd@gmail.com>
2024-12-08 15:37:06 +0900

    fix: type

--- src/context/ThemeContext.ts
+++ src/context/ThemeContext.ts
@@ -2,5 +2,5 @@ import { createContext, Dispatch, SetStateAction } from "react";
 
 export const ThemeContext = createContext<{
   theme: "dark" | "light";
-  setTheme: Dispatch<SetStateAction<string>>;
+  setTheme: Dispatch<SetStateAction<"dark" | "light">>;
 }>({ theme: "dark", setTheme: () => {} });
--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren, useState } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
 function ThemeProvider({ children }: PropsWithChildren) {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState<"dark" | "light">("dark");
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
