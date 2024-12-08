2b4b5976977bea791553cf5d2ddcc718a242293c
dltmdrbtjd <dltmdrbtjd@gmail.com>
2024-12-08 16:23:48 +0900

    remove unnecessary context provider

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -4,11 +4,7 @@ import { ThemeContext } from "../context/ThemeContext";
 function ThemeProvider({ children }: PropsWithChildren) {
   const [theme, setTheme] = useState<"dark" | "light">("dark");
 
-  return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+  return <ThemeContext value={{ theme, setTheme }}>{children}</ThemeContext>;
 }
 
 export default ThemeProvider;
