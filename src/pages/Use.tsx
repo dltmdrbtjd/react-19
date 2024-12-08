57d9c7eaa717b60db61427817878763e46b2fe0d
dltmdrbtjd <dltmdrbtjd@gmail.com>
2024-12-08 16:28:57 +0900

    Add Document Metadata

--- src/pages/Use.tsx
+++ src/pages/Use.tsx
@@ -10,6 +10,12 @@ function Use() {
 
   return (
     <ThemeProvider>
+      <head>
+        <title>제목</title>
+        <meta name="author" content="Winter is coming" />
+        <link rel="author" href="https://github.com/dltmdrbtjd" />
+        <meta name="keywords" content="키워드" />
+      </head>
       <ThemeButtons />
       <Heading>User List</Heading>
       <Suspense fallback={<div>Loading...</div>}>
